5efc04b509cc76d31a63ac8615bac5d77ded7dc4
RitankS <146180073+RitankS@users.noreply.github.com>
2024-05-13 17:44:47 +0530

    new changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const stripe = require("stripe")
 
 
 const PORT = process.env.PORT || 8080;
@@ -13,6 +14,45 @@ app.get('/', (req, res) => {
 app.get('/ping', (req, res) => {
     res.send('PONG')
 });
+
+app.post("/pay" , async(req,res)=>{
+    const STRIPE_KEY = "sk_test_51Nv0dVSHUS8UbeVicJZf3XZJf72DL9Fs3HP1rXnQzHtaXxMKXwWfua2zi8LQjmmboeNJc3odYs7cvT9Q5YIChY5I00Pocly1O1"
+    const { price, name, custName, email } = req.body;
+    const Stripe = new stripe(STRIPE_KEY);
+
+    try {
+        const newPrice = Math.ceil(parseFloat(price));
+      
+        const customer = await Stripe.customers.create({
+            name: custName,
+        });
+        const custId = customer.id
+        const myPrice = await Stripe.prices.create({
+            currency: 'INR',
+            unit_amount: newPrice,
+            product_data: {
+                name: name,
+            },
+        });
+
+        const priceId = myPrice.id;
+        const session = await Stripe.checkout.sessions.create({
+            success_url: 'https://example.com/success',
+            line_items: [
+                {
+                    price: priceId,
+                    quantity: 10,
+                },
+            ],
+            mode: 'payment',
+        });
+        res.status(200).json(({ priceId , email  , session}));
+    }
+    catch(err){
+        res.status(500).json(err.message)
+    }
+})
+
 
 app.listen(8080, () => {
     console.log('Server is listenin on PORT :' + PORT);
