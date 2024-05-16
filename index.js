d6b08ad0732314cf186280ef6250cc30dbd2ee7d
RitankS <146180073+RitankS@users.noreply.github.com>
2024-05-16 17:05:06 +0530

    chnages

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.get('/resource', async (req, res) => {
                         });
 
                         document.body.appendChild(fetchButton);
-                    }, 7000); // 10 seconds delay
+                    }, 6000); // 10 seconds delay
                 </script>
             </body>
             </html>
